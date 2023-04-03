affeba928d7471dad2a90ed1de3f8965fb572c2a
reshmalb <reshma.lb27@gmail.com>
2023-04-03 19:20:22 +0530

    auto logout

--- login/src/Store/AuthorizationContext.js
+++ login/src/Store/AuthorizationContext.js
@@ -11,18 +11,33 @@ const AuthorizationContext=React.createContext({
 export const AuthorizationProvider=(props)=>{
    const initial_state=localStorage.getItem('token')
    const [istoken,setToken]=useState(initial_state)
-
+    
+   const autoLogoutHandler=(timer)=>{
+      const interval= setTimeout(() => {
+        alert("Please Login .")
+        logoutHandler();
+        
+     }, timer);
+     return(()=>{
+        clearTimeout(interval)
+     })
+    }
+    if(initial_state){
+      autoLogoutHandler(5000)
+    }   
     const userIsLoggedin = !!istoken;//return true if token is string
                                   // and not empty
 
      const loginHandler=(token)=>{
         setToken(token)
         localStorage.setItem('token',token)
+       autoLogoutHandler(5000)
      }     
      const logoutHandler=()=>{
         setToken(null)
         localStorage.removeItem('token')
      }    
+  
      
      const context={
         token:istoken,
