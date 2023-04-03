8414c6fd32d5ff785b320ac4b0fbca23cb338257
reshmalb <reshma.lb27@gmail.com>
2023-04-03 11:40:24 +0530

    password handler

--- login/src/Pages/Profile.js
+++ login/src/Pages/Profile.js
@@ -1,9 +1,55 @@
-import React from "react";
+import React,{useContext, useRef} from "react";
+import AuthorizationContext from "../Store/AuthorizationContext";
+
+
+
 const Profile= ()=>{
-    return (<>
-    <p>
-        Profile
-    </p>
-    </>);
+    const ctx=useContext(AuthorizationContext)   ;
+
+    const inputpassword=useRef();
+    const onSubmitHandler=(e)=>{
+        e.preventDefault();
+        const password=inputpassword.current.value;
+
+        fetch("https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyD0an-iOy1im1Cjd3_OhzCjGooPUxdc7Es",
+        {
+            method:'POST',
+            body:JSON.stringify({
+                idToken:ctx.token,
+                password:password,
+                returnSecureToken:true
+
+            }),
+            headers:{
+                'Content-Type':'application/json'
+            }
+       }).then(resp=>{
+        if(resp.ok){
+            alert("password changed successfully...")
+        }
+       })
+    }
+    return (
+
+        <div  justifyContent="center">
+ <form style={{
+            width:"200px",
+            height:"300px",
+            marginTop:"100px",
+            marginleft:"100px",
+            marginRight:"50%",
+            alignContent:"center",
+            backgroundColor:"off-white",
+            justifyContent:"center"
+          
+    }}     onSubmit={onSubmitHandler}>
+        <label htmlFor="changepassword"> Enter New Password</label>
+        <input type="password" id="changepassworda" minLength="7" ref={inputpassword}></input>
+        <button>Change password</button>
+
+        </form>
+        </div>
+       
+        );
 }
 export default Profile;
